811247b1ee347ecdebe2c33ce4eef49451d6e0a0
Mayra Cruz <mbc15b@gmail.com>
2024-09-20 23:36:21 -0300

    Update ejercicio_4.py

--- ejercicio_4.py
+++ ejercicio_4.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
     # Ojo! Tener cuidado con lo string/caracteres
     # que no son números, utilizar condicionales para detectarlos
     # reemplazar dicho str "no numérico" por 0
+    # TIP: Recomendamos ver el método "isdigit" de strings
+    # para aplicar en este caso.
 
     lista_str = ['5', 'b', '3', '', '7', 'NaN', 'E', '0']
     lista_num = [int(x) if (x.isdigit()) else 0 for x in lista_str]
@@ -21,11 +23,13 @@ if __name__ == '__main__':
     # Prueba con número negativo
 
     lista_str = ['-5', 'b', '-3', '', '7', 'NaN', 'E', '0']
-
     lista_numeros = [int(x) if(x.isdigit()) else 0 for x in lista_str]
     print("Lista inicial con numeros negativos:", lista_str)
     print("Conversión de la lista:", lista_numeros)
 
     print('La función isdigit tomó los numeros negativos como string')
+
+    lista_numeros = [int(x) if(x.lstrip('-').isdigit()) else 0 for x in lista_str]
+    print('Si utilizo el método lstrip, la lista resultante será:', lista_numeros)
 
     print("Ejercicio 4 - Finalizado")
